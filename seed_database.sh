a2f2f206bb128673468116f092cfb65953c00a76
Tricia Swift <tricia.swift8@gmail.com>
2023-12-04 21:42:29 -0600

    Added fixtures for each model

---
+++ seed_database.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./tastevineapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations tastevineapi
+python3 manage.py migrate tastevineapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata ingredients
+python3 manage.py loaddata categories
+python3 manage.py loaddata recipes
+python3 manage.py loaddata recipecategories
+python3 manage.py loaddata recipeingredients
