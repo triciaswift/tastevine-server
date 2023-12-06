fdafa213f7a930de50de9d7ba0321873442b6d98
Tricia Swift <tricia.swift8@gmail.com>
2023-12-06 16:32:44 -0600

    Added list method to ingredients view

--- tastevineapi/views/ingredient_view.py
+++ tastevineapi/views/ingredient_view.py
@@ -1,4 +1,7 @@
-from rest_framework import serializers
+from django.http import HttpResponseServerError
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 from tastevineapi.models import Ingredient
 
 class IngredientSerializer(serializers.ModelSerializer):
@@ -6,4 +9,21 @@ class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ingredient
-        fields = ('name',)
+        fields = ('id', 'name',)
+
+
+class IngredientView(ViewSet):
+    """Ingredient view set"""
+
+    def list(self, request):
+        """Handle GET requests for all ingredients
+
+        Returns:
+            Response -- JSON serialized list of ingredients
+        """
+        try:
+            ingredients = Ingredient.objects.all()
+            serializer = IngredientSerializer(ingredients, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
--- tastevineproject/urls.py
+++ tastevineproject/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
-from tastevineapi.views import register_user, login_user, RecipeView
+from tastevineapi.views import register_user, login_user, RecipeView, IngredientView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"recipes", RecipeView, "recipe")
+router.register(r"ingredients", IngredientView, "ingredient")
 
 
 urlpatterns = [
