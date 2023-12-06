4603cce7ed2af093dad93763d7ed8fc0df4639a3
Tricia Swift <tricia.swift8@gmail.com>
2023-12-06 17:25:05 -0600

    Added create method for ingredient view

--- tastevineapi/views/ingredient_view.py
+++ tastevineapi/views/ingredient_view.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from django.db.models.functions import Lower
 from tastevineapi.models import Ingredient
 
 class IngredientSerializer(serializers.ModelSerializer):
@@ -22,8 +23,24 @@ class IngredientView(ViewSet):
             Response -- JSON serialized list of ingredients
         """
         try:
-            ingredients = Ingredient.objects.all()
+            ingredients = Ingredient.objects.all().order_by(Lower("name"))
             serializer = IngredientSerializer(ingredients, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized instance
+        """
+        ingredient = Ingredient()
+        ingredient.name = request.data["name"]
+
+        try:
+            ingredient.save()
+            serializer = IngredientSerializer(ingredient)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as ex:
+            return Response({"reason": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
